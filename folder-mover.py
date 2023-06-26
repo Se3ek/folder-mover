763d476dfad18edc1322847a737f49c87efc52bd
Sebastian Lufer <se3ek@proton.me>
2023-06-26 17:20:00 +0200

    initial setup

--- folder-mover.py
+++ folder-mover.py
@@ -1,7 +1,6 @@
-# folder-mover
-Moves files into folders of the same name
-
+"""
 Just a little program to move files into folders of the same name excluding the file extension.
 It is meant to sort multiple file extension versions of content into folders for better visibility.
 I want it to get the contents of the folder it's placed in, create a folder for the name of the file on top of the list,
 if it doesn't already exist, and move it into the folder.
+"""
