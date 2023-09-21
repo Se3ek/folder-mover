5104416b957f8acab9ea82d21e78bade2bad5463
Seb <Se3ek@proton.me>
2023-09-21 13:24:44 +0200

    Working as standalone with inputs
    Tested only on linux for now, Test on Windows necessary

--- folder-mover.py
+++ folder-mover.py
@@ -1,20 +1,36 @@
-"""
-Just a little program to move files into folders of the same name excluding the file extension.
-It is meant to sort multiple file extension versions of content into folders for better visibility.
-I want it to get the contents of the folder it's placed in, create a folder for the name of the file on top of the list,
-if it doesn't already exist, and move it into the folder.
-"""
+import os, shutil, sys
 
-import os, shutil
+def movefiles(directory):
+    """Actual part of the work to be done
 
-# List of to-be-ignored files (mainly for debugging purposes and for itself):
-ignore = ['.git', 'README.md', 'folder-mover.py', '.gitignore']
+    Args:
+        directory (str): Absolute directory path where the program should be executed
+    """
+        
+    # List of to-be-ignored files (mainly for debugging purposes and for itself):
+    ignore = ['.git', 'README.md', 'folder-mover.py', '.gitignore']
 
-# Create list of non-ignored filenames:
-files = [file for file in os.scandir() if file.name not in ignore]  # Comprehend a list of not ignored filenames
+    # Create list of non-ignored filenames:
+    files = [file for file in os.scandir(directory) if file.name not in ignore]  # Comprehend a list of not ignored filenames
 
-nameset = {os.path.splitext(file.name)[0] for file in files} # Make a set for for folders to be created
+    nameset = {os.path.splitext(file.name)[0] for file in files} # Make a set for for folders to be created
 
-for item in nameset: os.mkdir(item)  # Create directory for every name in the set to then move files into
+    for item in nameset: os.mkdir(os.path.join(directory, item))  # Create directory for every name in the set to then move files into
 
-for filetomove in files: shutil.move(filetomove, os.path.splitext(filetomove)[0])    # move files into their respective folders
+    for filetomove in files: shutil.move(filetomove, os.path.join(directory, os.path.splitext(filetomove)[0]))    # move files into their respective folders
+
+def main(input):
+    """Take the given input and call the work function to move contained files into folders of the name without extension
+
+    Args:
+        input (str): Either --here flag (Starts process in script's location) or absolute path of a directory
+    """
+    
+    # If the --here flag is called, get the current path and pass that into the work function
+    if input == "--here":
+        # Get the current path through the os package and overwrite with current working directory
+        input = os.getcwd()
+        
+    movefiles(input)  # Call the working function with the determined path string
+
+main(sys.argv[1])
