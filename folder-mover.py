e9d2aec96fd92a36c6b644a148bd9151e69bd7b8
Seb <Se3ek@proton.me>
2023-09-21 16:22:04 +0200

    Working version with argument parser

--- folder-mover.py
+++ folder-mover.py
@@ -1,14 +1,12 @@
-import os, shutil, sys
+import os, shutil, argparse
 
-def movefiles(directory):
+def movefiles(directory, ignore):
     """Actual part of the work to be done
 
     Args:
         directory (str): Absolute directory path where the program should be executed
+        ignore (list): List of to be ignored files
     """
-        
-    # List of to-be-ignored files (mainly for debugging purposes and for itself):
-    ignore = ['.git', 'README.md', 'folder-mover.py', '.gitignore']
 
     # Create list of non-ignored filenames:
     files = [file for file in os.scandir(directory) if file.name not in ignore]  # Comprehend a list of not ignored filenames
@@ -19,18 +17,31 @@ def movefiles(directory):
 
     for filetomove in files: shutil.move(filetomove, os.path.join(directory, os.path.splitext(filetomove)[0]))    # move files into their respective folders
 
-def main(input):
+def main(args):
     """Take the given input and call the work function to move contained files into folders of the name without extension
 
     Args:
-        input (str): Either --here flag (Starts process in script's location) or absolute path of a directory
+        args: Parsed arguments from Arguments Parser to be analyzed on what to do
     """
     
-    # If the --here flag is called, get the current path and pass that into the work function
-    if input == "--here":
-        # Get the current path through the os package and overwrite with current working directory
-        input = os.getcwd()
+    # Path string
+    path = args.path
+    
+    # List of to-be-ignored files (mainly for debugging purposes and for itself):
+    ignore = ['.git', 'README.md', 'folder-mover.py', '.gitignore']
         
-    movefiles(input)  # Call the working function with the determined path string
+    movefiles(path, ignore)  # Call the working function with the determined path string
+    
+# Initialize a new parser for the CLI
+parser = argparse.ArgumentParser(description='Moves files of a given directory into folders with the same name without the extension')
 
-main(sys.argv[1])
+# store the path as a positional argument
+parser.add_argument('-p', '--path',
+                    type=str,
+                    default=os.getcwd(),
+                    help='Path for the script to work in. Default is the working directory from where the script is called'                    
+                    )
+
+args = parser.parse_args()  # Get the arguments given by the caller
+
+main(args)
