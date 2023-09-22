0db204ed3e47aed1e1ad9f7ddf1290ef2d99993d
Seb <Se3ek@proton.me>
2023-09-22 11:39:46 +0200

    Added custom ignore options

--- folder-mover.py
+++ folder-mover.py
@@ -5,7 +5,7 @@ def movefiles(directory, ignore):
 
     Args:
         directory (str): Absolute directory path where the program should be executed
-        ignore (list): List of to be ignored files
+        ignore (list): List of to be ignored files (file name with extension, no wildcards)
     """
 
     # Create list of non-ignored filenames:
@@ -23,24 +23,58 @@ def main(args):
     Args:
         args: Parsed arguments from Arguments Parser to be analyzed on what to do
     """
-    
+        
     # Path string
     path = args.path
     
-    # List of to-be-ignored files (mainly for debugging purposes and for itself):
-    ignore = ['.git', 'README.md', 'folder-mover.py', '.gitignore']
+    # List of default to-be-ignored files (mainly for debugging purposes and for itself):
+    ignore = ['.git', 'README.md', 'folder-mover.py', '.gitignore', 'Test.sh']
+    
+    # Add custom ignore list to the default list if there is one
+    if args.ignore:
+        ignore += args.ignore.split(",")
+    
+    # Overwrite the ignore list with the exclusive ignore list if there is one
+    if args.ignore_only:
+        ignore = args.ignore_only.split(",")
         
-    movefiles(path, ignore)  # Call the working function with the determined path string
+    # If required, log the ignore list
+    if args.verbose_ignore:
+        print(ignore)
+    else:
+        movefiles(path, ignore)  # Call the working function with the determined path string    
     
 # Initialize a new parser for the CLI
 parser = argparse.ArgumentParser(description='Moves files of a given directory into folders with the same name without the extension')
 
-# store the path as a positional argument
-parser.add_argument('-p', '--path',
-                    type=str,
-                    default=os.getcwd(),
-                    help='Path for the script to work in. Default is the working directory from where the script is called'                    
-                    )
+# store the path as a variable
+parser.add_argument(
+    '-p', '--path',
+    type = str,
+    default = os.getcwd(),
+    help = 'Path for the script to work in. Default is the working directory from where the script is called'
+)
+
+# store the custom ignore list
+parser.add_argument(
+    '-i', '--ignore',
+    type = str,
+    help = 'Comma-separated list of files to be ignored by the script (with file extensions) including list: [".git", "README.md", "folder-mover.py", ".gitignore", "Test.sh"]'
+)
+
+# store the exclusive custom ignore list
+parser.add_argument(
+    '-io', '--ignore_only',
+    type = str,
+    help = 'Comma-separated list of files to be ignored by the script (with file extensions). Overgoes the default list.'
+)
+
+# See if the ignore list is supposed to be logged for testing
+parser.add_argument(
+    '-vi', '--verbose_ignore',
+    action = 'store_true',
+    help = argparse.SUPPRESS
+)
 
 args = parser.parse_args()  # Get the arguments given by the caller
 
